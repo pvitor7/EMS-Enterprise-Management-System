b1de0a251ace33791d222ab46de954f1933f91bd
Paulo Vitor <pvitor7@hotmail.com>
2023-04-12 18:49:00 -0300

    chore: url projects updated for include param departament_id

--- projects/urls.py
+++ projects/urls.py
@@ -3,6 +3,6 @@ from .views import ProjectListCreateView, ProjectRetrieveUpdateDestroyView
 from django.urls import path
 
 urlpatterns = [
-    path("", ProjectListCreateView.as_view(), name="projects-list-create-view"),
-    path("<pk>/", ProjectRetrieveUpdateDestroyView.as_view(), name="project-retrive-view"),
+    path("departaments/<str:departament_id>/projects/", ProjectListCreateView.as_view(), name="projects-list-create-view"),
+    path("departaments/<str:departament_id>/projects/<pk>/", ProjectRetrieveUpdateDestroyView.as_view(), name="project-retrive-view"),
 ]   
