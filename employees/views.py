ed3fd5075cae0585935e2494286202b31786f200
Paulo Vitor <pvitor7@hotmail.com>
2023-04-13 18:21:28 -0300

    feat: created view for include employees in departaments with SerializerByMethodMixin

--- employees/views.py
+++ employees/views.py
@@ -1,7 +1,9 @@
 from .models import Employees
-from .serializers import EmployeeSerializer
+from .serializers import EmployeeSerializer, DepartamentEmployeeSerializer, GETDepartamentEmployeeSerializer
+from departaments.models import Roles
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
+from .utils import SerializerByMethodMixin
 
 # Create your views here.
 class EmployeeListCreateView(generics.ListCreateAPIView):
@@ -14,3 +16,22 @@ class EmployeeRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
     queryset = Employees.objects.all()
     serializer_class = EmployeeSerializer
+
+
+class DepartamentEmployeeView(SerializerByMethodMixin, generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    queryset = Roles.objects.all()
+    serializer_map = {
+        'GET': GETDepartamentEmployeeSerializer,
+        'POST': DepartamentEmployeeSerializer
+    }
+    
+
+class DepartamentEmployeeIDView(SerializerByMethodMixin, generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    queryset = Roles.objects.all()
+    serializer_map = {
+        'GET': GETDepartamentEmployeeSerializer,
+        'PATCH': DepartamentEmployeeSerializer,
+        'DELETE': DepartamentEmployeeSerializer
+    }
