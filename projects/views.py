92640c86841a717e3e266ef47e185792a070a7f4
Paulo Vitor <pvitor7@hotmail.com>
2023-04-17 15:21:26 -0300

    chore: include view for relation create  employee and project

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Project, ProjectsEmployees
+from .serializers import ProjectSerializer, ProjectEmployeeSerializer
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 
@@ -14,3 +14,10 @@ class ProjectRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [TokenAuthentication]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    
+    
+class ProjectsEmployeeListCreateView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    queryset = ProjectsEmployees.objects.all()
+    serializer_class = ProjectEmployeeSerializer
+    
