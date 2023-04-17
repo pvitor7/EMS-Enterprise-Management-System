25109fa09f960260b131a428db6d63fc841af6a7
Paulo Vitor <pvitor7@hotmail.com>
2023-04-17 15:20:09 -0300

    chore: include url for relation create  employee and project

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 
-from .views import ProjectListCreateView, ProjectRetrieveUpdateDestroyView
+from .views import ProjectListCreateView, ProjectRetrieveUpdateDestroyView, ProjectsEmployeeListCreateView
 from django.urls import path
 
 urlpatterns = [
-    path("departaments/<str:departament_id>/projects/", ProjectListCreateView.as_view(), name="projects-list-create-view"),
+    path("departaments/<str:departament_id>/projects/<pk>/employees/", ProjectsEmployeeListCreateView.as_view()),
     path("departaments/<str:departament_id>/projects/<pk>/", ProjectRetrieveUpdateDestroyView.as_view(), name="project-retrive-view"),
+    path("departaments/<str:departament_id>/projects/", ProjectListCreateView.as_view(), name="projects-list-create-view"),
 ]   
